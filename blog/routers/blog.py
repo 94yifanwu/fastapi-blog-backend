fc926858337c664e923a24ae7b753f50afd3bc59
Yifan Wu <94yifanwu@gmail.com>
2021-06-17 02:25:54 -0700

    update

---
+++ blog/routers/blog.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, status
+from .. import database, schemas
+from sqlalchemy.orm import Session
+from ..repository import blog
+from typing import List
+
+router = APIRouter(
+    prefix="/blog",
+    tags=['Blogs']
+)
+
+get_db = database.get_db
+
+
+@router.get('/', response_model=List[schemas.ShowBlogResponse])
+def all(db: Session = Depends(get_db)):
+    return blog.get_all(db)
+
+
+@router.get('/{id}', status_code=200, response_model=schemas.ShowBlogResponse)
+def show(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    return blog.show(id, db)
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    # add user id
+    return blog.create(request, db)
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    return blog.destroy(id, db)
+
+
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    return blog.update(id, request, db)
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,55 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
+    tytle: str
+    body: str
+
+
+class Blog(BlogBase):
+    class Config():
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class User(BlogBase):
+    class Config():
+        orm_mode = True
+
+
+class ShowUserResponse(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog] = []
+
+    class Config():
+        orm_mode = True
+
+
+class ShowBlogResponse(BaseModel):
     title: str
     body: str
+    creator: ShowUserResponse
+
+    class Config():
+        orm_mode = True
+
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
