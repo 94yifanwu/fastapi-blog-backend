e52cdf7d6f813c602feb9ea15133d9ed1660239b
Yifan Wu <94yifanwu@gmail.com>
2021-06-17 02:35:25 -0700

    update

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status
-from .. import database, schemas
+from .. import database, schemas, oauth2
 from sqlalchemy.orm import Session
 from ..repository import blog
 from typing import List
@@ -13,26 +13,26 @@ get_db = database.get_db
 
 
 @router.get('/', response_model=List[schemas.ShowBlogResponse])
-def all(db: Session = Depends(get_db)):
+def all(current_user: schemas.User = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
     return blog.get_all(db)
 
 
 @router.get('/{id}', status_code=200, response_model=schemas.ShowBlogResponse)
-def show(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+def show(id: int, request: schemas.Blog, current_user: schemas.User = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
     return blog.show(id, db)
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create(request: schemas.Blog, current_user: schemas.User = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
     # add user id
     return blog.create(request, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(get_db)):
+def destroy(id: int, current_user: schemas.User = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
     return blog.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+def update(id: int, request: schemas.Blog, current_user: schemas.User = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
     return blog.update(id, request, db)
